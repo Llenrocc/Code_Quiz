5d3621ebedbde8d7557287cded6a2e1270bec579
LLenroc <nc_2@hotmail.com>
2020-06-17 20:53:03 -0400

    starting to add question portions in javasacript

--- script.js
+++ script.js
@@ -10,4 +10,12 @@ var opt3 = document.getElementById('opt3');
 var opt4 = document.getElementById('opt4');
 var totQuestions = questions.length;
 
-var nex
+var nextButton = document.getElementById('nextButton');
+var resultCont = document.getElementById('result');
+
+// Adding load question function 
+function loadQuestion (questionIndex) {
+    var q = questions[questionIndex];
+    questionEl.textContent = (questionIndex + 1) + '. ' + q.question;
+    opt1.textContent = q.
+}
